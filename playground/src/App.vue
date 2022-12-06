79f7caac096935fb8fb1c01507f42c676bcaff7a
Simon He <13917107469@163.com>
2022-12-06 17:00:02 +0800

    chore: update

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import { animateImage } from '../../src'
+import { animateImage } from "../../src";
 
 animateImage({
   images: [
-    new URL('../public/1.png', import.meta.url).href,
-    new URL('../public/2.png', import.meta.url).href,
-    new URL('../public/3.png', import.meta.url).href,
-    new URL('../public/4.png', import.meta.url).href,
-    new URL('../public/5.png', import.meta.url).href,
-    new URL('../public/6.png', import.meta.url).href,
-    new URL('../public/7.png', import.meta.url).href,
-    new URL('../public/8.png', import.meta.url).href,
-    new URL('../public/9.png', import.meta.url).href,
-    new URL('../public/10.png', import.meta.url).href,
-    new URL('../public/11.png', import.meta.url).href,
+    new URL("../public/1.png", import.meta.url).href,
+    new URL("../public/2.png", import.meta.url).href,
+    new URL("../public/3.png", import.meta.url).href,
+    new URL("../public/4.png", import.meta.url).href,
+    new URL("../public/5.png", import.meta.url).href,
+    new URL("../public/6.png", import.meta.url).href,
+    new URL("../public/7.png", import.meta.url).href,
+    new URL("../public/8.png", import.meta.url).href,
+    new URL("../public/9.png", import.meta.url).href,
+    new URL("../public/10.png", import.meta.url).href,
+    new URL("../public/11.png", import.meta.url).href,
   ],
-  container: '.main',
-})
+  container: ".main",
+  infinity: true,
+});
 </script>
 
 <template>
